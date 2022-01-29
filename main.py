22daf9cd0265e2f55bf1ec0a638e80b974792ae2
Tatsuya Mizuguchi <mizuguchi_yho@yahoo.co.jp>
2022-01-29 15:51:02 +0900

    init

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+from tkinter import scrolledtext
+
+
+def analyze(sensor):
+    info = {
+        "Accelerometer": {"Avg": 0, "Max": 0, "Min": 0},
+        "Gyro": {"Avg": 0, "Max": 0, "Min": 0}
+    }
+    info = pd.DataFrame(info)
+
+    return info
+
+
+def main(log):
+    file = filedialog.askopenfile(initialdir='~/')
+    if file:
+        try:
+            sensor = pd.read_csv(file.name)
+            outfile = os.path.splitext(file.name)[0] + "_analyzed.csv"
+        except Exception as e:
+            print(e)
+            log.insert("end", "ファイルが開けません\n")
+            return
+
+        try:
+            info = analyze(sensor)
+            log.insert("end", f"解析完了\n解析ファイル：{outfile}\n")
+            info.to_csv(outfile)
+        except Exception as e:
+            print(e)
+            log.insert("end", "ファイルの中身が不正です\n")
+
+        file.close()
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title('Sensor Analyzer')
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+    frame = ttk.Frame(root, padding=10)
+    frame.columnconfigure(0, weight=1)
+    frame.rowconfigure(0, weight=1)
+    frame.grid(sticky=N + W + S + E)
+
+    # Log
+    log = scrolledtext.ScrolledText(frame, height=10)
+    log.grid(row=1, column=0, sticky=W)
+
+    # Analyze
+    b = ttk.Button(
+        frame, text='Analyze Sensor File', width=15,
+        command=lambda: main(log))
+    b.grid(row=0, column=0, sticky='')
+
+    root.mainloop()
